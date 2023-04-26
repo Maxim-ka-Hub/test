0fbbb424386a0f4f2d977bea5d405437a9ea32e2
Maxim <donator.man8@gmail.com>
2023-04-26 16:18:43 +0300

    new

---
+++ Script.js
@@ -0,0 +1,9 @@
+let value_1 = parseInt(prompt("Введите первое число:"))
+let value_2 = parseInt(prompt("Введите второе число:"))
+
+if (value_1 > value_2)
+{alert("Первое число больше чем второе: " + value_1);}
+
+else if (value_2 > value_1)
+{alert("Второе число больше чем первое: " + value_2);}
+
